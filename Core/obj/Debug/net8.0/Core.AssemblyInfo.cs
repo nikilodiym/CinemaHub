18d0a7d5236f052598eebd630f30bceab0b39a8d
Nikita Krenov <nikitabaram@gmail.com>
2025-05-20 15:57:53 +0200

    Now this project is running. Hurray the package was in the right direction because the startup file, althought it was written correctly, there was no folder where it was located that is the path now everything is okay

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee2774a3505a1aea0d63e6cebc4db206e6a045f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19671d537c32923a195f5081ab4378f839d6fad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
