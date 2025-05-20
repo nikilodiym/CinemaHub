160183608ec87c42448523eb2c8716f15b983737
Nikita Krenov <nikitabaram@gmail.com>
2025-05-20 20:34:51 +0200

    corrected a little

--- CinemaWPF/Views/Auth/LoginView.xaml.cs
+++ CinemaWPF/Views/Auth/LoginView.xaml.cs
@@ -14,21 +14,17 @@ public partial class LoginView : UserControl
     {
         MessageBox.Show("Welcome to user mode! Movies loading...");
 
-        // Відкриваємо вікно з користувацьким інтерфейсом
         var userWindow = new UserWindow();
         userWindow.Show();
 
-        // Закриваємо батьківське вікно
         Window.GetWindow(this)?.Close();
     }
 
     private void AdminButton_Click(object sender, RoutedEventArgs e)
     {
-        // Відкриваємо вікно з адмінським інтерфейсом
         var adminWindow = new AdminWindow();
         adminWindow.Show();
 
-        // Закриваємо батьківське вікно
         Window.GetWindow(this)?.Close();
     }
 
