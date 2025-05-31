775a8a3a8c9d34ca855fc5cc6658fa59da39c71d
ForgeDom <maxstill2006@gmail.com>
2025-05-31 12:25:11 +0300

    +

--- Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6684e41413e2214ef5e8dfc35fe0b49b436d90f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53e3e7766477db99dc46661d244b4eca426b14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
